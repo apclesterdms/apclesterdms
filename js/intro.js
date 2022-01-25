e3b0c8919fc898da380cebe8abf42c0b5d2c0f19
Lester Dave Salazar <97135826+apclesterdms@users.noreply.github.com>
2022-01-25 15:10:30 +0800

    Add files via upload

---
+++ js/intro.js
@@ -0,0 +1,13 @@
+// scroll reveal
+ScrollReveal({
+    reset: true,
+    distance: '50px',
+    duration: 2500,
+    delay: 50
+});
+
+ScrollReveal().reveal('.introduction', { delay: 400, origin: 'left' });
+ScrollReveal().reveal('.highlight', { delay: 400, origin: 'left' });
+ScrollReveal().reveal('.tab', { delay: 600, origin: 'left' });
+ScrollReveal().reveal('.lester', { delay: 600, origin: 'left' });
+ScrollReveal().reveal('.blob', { delay: 600, origin: 'right' });
